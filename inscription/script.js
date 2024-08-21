7d48ce173dd3d04dd006a3e5b278649afbc83c16
TINDANO33 <tindanodesign@gmail.com>
2024-08-21 16:17:59 +0000

    inscription

--- inscription/script.js
+++ inscription/script.js
@@ -1,22 +1,106 @@
-function soumission(){
+const nombres = ['0','1','2','3','4','5','6','7','8','9'];
 
-    let infos ={}
-    /* Les variables des informations personnelles*/
-    let nom = document.querySelector("#nom");
-    let prenom = document.querySelector("#prenom");
-    let mdp = document.querySelector("#password");
-    let mdpConfirm = document.querySelector("#passwordConfirm");
+const countrySelect = document.getElementById('country-select');
+const phoneInput = document.getElementById('phone-input');
+const submitBtn = document.getElementById('submit-btn');
 
-    // Vérification de nom et prénom
-   if (nom.value == ""){
-    nom.style.border = "solid red 2px";
-   }else if (prenom.value == ""){
-    prenom.style.border = "solid red 2px";
-   } else if (mdp.value.length < 6){
-   mdp.style.border = "solid red 2px";
-   }else if(mdpConfirm.value !== mdp.value){
-    mdpConfirm.style.border = "solid red 2px";
-   }else{
-   document.querySelector(".container").style.display = "none";
-   }
+function nameVerify(){
+    var name = document.querySelector("#nom");
+    var letters = name.value.split("");
+    for (const i of letters){
+        if (nombres.includes(i)){
+            name.style.backgroundColor = "#ffc1c1";
+            break
+        }else {name.style.backgroundColor = "#d4fbef";}
+    }
+    
 }
+function prenomVerify(){
+    var prenm = document.querySelector("#prenom");
+    var letters = prenm.value.split("");
+    for (const i of letters){
+        if (nombres.includes(i)){
+            prenm.style.backgroundColor = "#ffc1c1";
+            break
+        }else {prenm.style.backgroundColor = "#d4fbef";}
+    }
+    
+}
+function numberVerify(){
+    var phone = document.querySelector("#phone");
+    var letters = phone.value.split("");
+    for (const i of letters){
+        if (!nombres.includes(i)){
+            phone.style.backgroundColor = "#ffc1c1";
+            break
+        }else {phone.style.backgroundColor = "#d4fbef";}
+    }
+    
+}
+
+function soumission(){
+    var nom = document.querySelector("#nom");
+    if (nom.value == ""){
+        nom.style.backgroundColor = "#ffc1c1";
+        return false
+    }else {prenmVerify()}
+}
+function prenmVerify(){
+    var prenm = document.querySelector("#prenom");
+    if (prenm.value == ""){
+        prenm.style.backgroundColor = "#ffc1c1";
+        return false
+    }else {pwdVerify()}
+}
+function pwdVerify(){
+    var pwd = document.querySelector("#password");
+    if (pwd.value == ""){
+        pwd.style.backgroundColor = "#ffc1c1";
+        return false
+    }else {pwdConfirmVerify()}
+}
+function pwdConfirmVerify(){
+    var pwd = document.querySelector("#password");
+    var pwdcfm = document.querySelector("#passwordConfirm");
+    if (pwd.value !== pwdcfm.value){
+        pwdcfm.style.backgroundColor = "#ffc1c1";
+        return false
+    }else {valider()}
+}
+
+
+function valider(){
+    document.querySelector(".container").style.display = "none";
+    document.querySelector(".success").style.display = "flex";
+}
+
+// Fonction pour nettoyer le numéro de téléphone en supprimant les espaces et les tirets
+function cleanPhoneNumber(phoneNumber) {
+    return phoneNumber.replace(/[\s-]/g, '');
+}
+
+// Quand un pays est sélectionné, mettre à jour l'input avec le code du pays
+countrySelect.addEventListener('change', function() {
+    const code = this.value;
+    phoneInput.value = code + ' ' + cleanPhoneNumber(phoneInput.value.replace(/^\+\d+/, ''));
+});
+
+// Quand l'utilisateur tape un code de pays, sélectionner automatiquement le pays correspondant
+phoneInput.addEventListener('input', function() {
+    const cleanedValue = cleanPhoneNumber(this.value);
+    const matchingOption = Array.from(countrySelect.options).find(option => cleanedValue.startsWith(option.value));
+    if (matchingOption) {
+        countrySelect.value = matchingOption.value;
+    }
+});
+
+// Gestion du clic sur le bouton de validation
+submitBtn.addEventListener('click', function() {
+    const cleanedPhoneValue = cleanPhoneNumber(phoneInput.value);
+    if (/^\+\d{1,3}\d{4,14}$/.test(cleanedPhoneValue)) {
+        soumission();
+    } else {
+        alert('Veuillez entrer un numéro de téléphone valide.');
+    }
+});
+
